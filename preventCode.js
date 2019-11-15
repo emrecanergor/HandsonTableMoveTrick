0891d92e4ab60dbfd4017dc6cc5986ee0a634e03
Emre Can <emrecan.ergor@gmail.com>
2019-11-15 15:53:47 +0300

    Create preventCode.js
    Added prevent code

---
+++ preventCode.js
@@ -0,0 +1,35 @@
+//open a div with 'hot' id in html body tag
+
+var hotElement = document.querySelector('#hot'), hot, hotSettings;
+hotSettings={data:{},columns:[]}; //fill properties
+                
+                hot = new Handsontable(hotElement, hotSettings);
+
+              hot.addHook('beforeKeyDown', function (event) {
+                    var editor = this.getActiveEditor();
+                    var selected = this.getSelected();
+                    var isEditMode;
+                    if (editor === undefined) {
+                        isEditMode = false;
+                    }
+                    else {
+                        isEditMode = this.getActiveEditor()._opened;
+                    }
+
+                    if (isEditMode) return;
+
+                    var endRowNum = selected ? (selected[0][2]) : null,
+                        endColNum = selected ? (selected[0][3]) : null,
+                        
+                        rowsNum = this.countRows(),
+                        colsNum = this.countCols(),
+                        
+                        isFirstCol = endColNum == 0,
+                        isLastCol = endColNum == (colsNum - 1);
+
+                    if (!isEditMode) {
+                        if ((isFirstCol && event.key == "ArrowLeft") || (isLastCol && event.key == "ArrowRight")) {
+                            Handsontable.Dom.stopImmediatePropagation(event);
+                        }
+                    }
+                });
